8e2f365e7688524737cbdf088bbd8163ade4be10
Asmaa <asmaa.abdelnasser2050@gmail.com>
2023-08-09 01:43:55 +0300

    rsolve deploy

--- src/app/shared/components/hero/hero.component.ts
+++ src/app/shared/components/hero/hero.component.ts
@@ -8,10 +8,10 @@ import { Component } from '@angular/core';
 })
 export class HeroComponent {
   slidesStore : string[] = [
-    "./../../../assets/imgs/hero.jpeg" ,
-    "./../../../assets/imgs/hero2.jpeg" ,
-    "./../../../assets/imgs/hero3.jpeg" ,
-    "./../../../assets/imgs/hero4.jpeg"
+    "../../../../assets/imgs/hero.jpeg" ,
+    "../../../../assets/imgs/hero2.jpeg" ,
+    "../../../../assets/imgs/hero3.jpeg" ,
+    "../../../../assets/imgs/hero4.jpeg"
   ];
   customOptions: OwlOptions = {
     loop: true,
