2c3a74ae92bb5dcbde5738081100b286ab6d61a2
Asmaa <asmaa.abdelnasser2050@gmail.com>
2023-08-09 19:52:53 +0300

    deploy

--- src/app/shared/components/hero/hero.component.ts
+++ src/app/shared/components/hero/hero.component.ts
@@ -24,7 +24,7 @@ export class HeroComponent {
     navText: ['', ''],
     nav: true ,
     autoplay : true ,
-    autoplayTimeout : 5000 ,
+    autoplayTimeout : 2500 ,
     autoplaySpeed : 1000 ,
     responsive: {
       0: {
